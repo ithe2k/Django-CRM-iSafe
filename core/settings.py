34d6736e0057106ea7a943175c653600bf52a16d
ithe2k <ithe2k@gmail.com>
2026-05-26 23:59:39 +0200

    Ajustes de configuracion previo deploy en Render

--- core/settings.py
+++ core/settings.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 
+import dj_database_url
 from django.utils.translation import gettext_lazy as _
 from dotenv import load_dotenv
 
@@ -71,14 +72,12 @@ WSGI_APPLICATION = "core.wsgi.application"
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
-    }
+    "default": dj_database_url.config(
+        # Lee la URL de la base de datos desde el entorno de Render
+        default=os.environ.get("DATABASE_URL"),
+        # Mantiene las conexiones abiertas 10 minutos para mejorar el rendimiento
+        conn_max_age=600,
+    )
 }
 
 
