deb4fcf096b822a59deae968ef83f5df2527e4b6
B.Diallo <ithe2k@gmail.com>
2026-05-28 01:06:30 +0200

    Update wsgi.py

--- core/wsgi.py
+++ core/wsgi.py
@@ -3,17 +3,15 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-# Levantar la aplicación de Django obligatoria
 application = get_wsgi_application()
 
-# --- PARCHE DE EMERGENCIA DE GUERRILLA ---
+# --- CARGA EXCLUSIVA DE USUARIOS LIMPIOS ---
 try:
     from django.core.management import call_command
-    print("🤖 INTENTANDO INYECTAR DATOS DESDE WSGI...")
-    call_command('loaddata', 'datos_produccion.json')
-    print("🎉 ¡ÉXITO TOTAL! DATOS CARGADOS EN NEON.")
+    print("🤖 INYECTANDO EQUIPO DE VENTAS EN NEON...")
+    call_command('loaddata', 'users/fixtures/test_users.json')
+    print("🎉 ¡ÉXITO TOTAL! JOAN, MARTA, PAU Y RAQUEL ESTÁN DENTRO.")
 except Exception as e:
     import traceback
-    print("❌ ERROR CRÍTICO CARGANDO JSON:")
+    print("❌ ERROR EN LA INYECCIÓN:")
     print(traceback.format_exc())
-# ------------------------------------------
