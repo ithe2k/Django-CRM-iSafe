a7588a304ac586180da29ad6d089967228417f43
B.Diallo <ithe2k@gmail.com>
2026-05-28 00:47:20 +0200

    fix: force loaddata inside wsgi

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,16 +1,19 @@
-"""
-WSGI config for core project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
+# Levantar la aplicación de Django obligatoria
 application = get_wsgi_application()
+
+# --- PARCHE DE EMERGENCIA DE GUERRILLA ---
+try:
+    from django.core.management import call_command
+    print("🤖 INTENTANDO INYECTAR DATOS DESDE WSGI...")
+    call_command('loaddata', 'datos_produccion.json')
+    print("🎉 ¡ÉXITO TOTAL! DATOS CARGADOS EN NEON.")
+except Exception as e:
+    import traceback
+    print("❌ ERROR CRÍTICO CARGANDO JSON:")
+    print(traceback.format_exc())
+# ------------------------------------------
